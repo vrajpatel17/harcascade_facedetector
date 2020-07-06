ed8c4a5dedc4aefc656a0c5c4d71359fe4a7828c
Vraj Patel <32016207+vrajpat17@users.noreply.github.com>
2020-07-06 19:55:01 +0530

    Add files via upload

---
+++ face detect01.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 14:14:54 2020
+
+@author: vrajp
+"""
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread("pic.jpg")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+    
+cv2.imshow('img', img)
+
+cv2.waitKey()
